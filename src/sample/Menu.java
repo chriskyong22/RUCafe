f52a74f49e870860d7ddda12377f9288b4f806aa
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2021-03-24 15:56:04 -0400

    Added Stage Switching for menu

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -1,8 +1,50 @@
 package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * TO ADD: DESCRIPTION
  * @author Christopher Yong, Maya Ravichandran
  */
 public class Menu {
 
+    public void handleCoffeeOrdering() {
+        createNewStage("Coffee.fxml", "RUCAFE: Coffee Menu");
+    }
+
+    public void handleDonutOrdering() {
+        createNewStage("Donut.fxml", "RUCAFE: Donut Menu");
+    }
+
+    public void handleCheckCurrentOrder() {
+        createNewStage("CurrentOrder.fxml", "RUCAFE: Current Order");
+    }
+
+    public void handleCheckOrders() {
+        createNewStage("StoreOrders.fxml", "RUCAFE: All Orders");
+    }
+
+    private void createNewStage(String path, String title) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(path));
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(new Scene(root, 800, 600));
+            stage.show();
+        } catch (IOException | NullPointerException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("RUCAFE: ERROR");
+            alert.setHeaderText("An exception has occurred!");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+    }
 }
